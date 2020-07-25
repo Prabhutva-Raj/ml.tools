37972ed1b7a7462c6087ab604c636192c5352c3e
Prabhutva-Raj <40884972+Prabhutva-Raj@users.noreply.github.com>
2020-07-25 15:02:57 +0530

    Add files via upload

---
+++ model_deploy/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+'''
+class RunDB(models.Model):
+   TASK = models.IntegerField()
+   CSV_FILE = models.FileField(upload_to = 'database')
+   DEP_VAR = models.IntegerField()
+
+   class Meta:
+      db_table = "run_db"
+'''
